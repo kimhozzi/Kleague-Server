9ea3a1bbdc8be5515e53b4f6733c2c9678fd735f
najongjine <najongjin3@hotmail.com>
2025-10-14 11:29:21 +0900

    .

--- app.py
+++ app.py
@@ -7,7 +7,31 @@ from typing import List, Dict, Any, Union, Optional
 
 import os # 환경 변수를 읽기 위해 os 모듈 추가
 
-app = FastAPI()
+# database_conn.py에 정의된 get_db_connection 함수를 임포트합니다.
+from database_conn import connect_to_db, close_db_connection, get_db_connection
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        await connect_to_db()
+    except Exception as e:
+        print(f"!!! [Startup] DB Connection FAILED: {e!r}")
+        # DB 연결 실패 시에도 서버를 띄울지 여부는 서비스 정책에 따라 결정
+    
+    
+    # --- 서버 실행 시작 (Yield) ---
+    yield 
+    
+    # --- 서버 종료 시 (Shutdown Event) ---
+    await close_db_connection()
+    print(">>> [Shutdown] FastAPI Server graceful shutdown complete.")
+
+app = FastAPI(
+    title="fastapi test",
+    description="fastapi test",
+    version="1.0.0",
+    lifespan=lifespan # <-- 여기에서 lifespan 함수를 등록합니다.
+)
 
 @app.get("/")
 def default_get(mydata: Optional[str] = None,
